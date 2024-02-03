39a817937ada85e9b93df5a52832ab30e9a915b0
cgzmartinez <105771374+cgzmartinez@users.noreply.github.com>
2024-02-02 20:52:46 -0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,17 @@
-/** @type {import('tailwindcss').Config} */
-export const content = ["./src/**/*.{html,js}"];
-export const theme = {
-  extend: {},
-};
-export const plugins = [];
+import withMT from '@material-tailwind/react/utils/withMT'
+
+export default withMT({
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+    'path-to-your-node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}',
+    'path-to-your-node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    fontFamily: {
+      sans: ['Roboto'],
+    },
+    extend: {},
+  },
+  plugins: [],
+})
