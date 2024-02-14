70bb8de83fbbd93e606f03dfc21f9428d1b6ecd8
cgzmartinez <105771374+cgzmartinez@users.noreply.github.com>
2024-02-14 13:47:58 -0600

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <div className="flex justify-center items-center bg-[#FD4556] mt-20">
+      <p className="p-5 text-[10px] text-left text-white">
+        This project site is not endorsed by Riot Games in any way. Riot Games,
+        Valorant, and all associated properties are trademarks or registered
+        trademarks of Riot Games, Inc.
+      </p>
+    </div>
+  )
+}
+
+export default Footer
