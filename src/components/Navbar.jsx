b30cbe05abba7e98e4f10e864f523ffb53767d8d
cgzmartinez <105771374+cgzmartinez@users.noreply.github.com>
2024-02-16 14:44:41 -0600

    Update Navbar.jsx
    Finalized Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,34 @@
-import Logo from '../assets/valorant-logo.svg?react'
+import { SiValorant } from 'react-icons/si'
+import { SiGithub } from 'react-icons/si'
 import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <nav className="fixed top-0 left-0 right-0 bg-[#1e1e1e] pb-5 z-50">
+    <nav className="fixed top-0 left-0 right-0 bg-[#1e1e1e] z-50">
       <div className="flex justify-center items-center">
+        <a
+          href="https://github.com/cgzmartinez/valorant-react-app"
+          target="_blank"
+        >
+          <SiGithub className="absolute top-9 left-10 h-7 md:h-10 w-auto fill-white/30 hover:fill-[#FD4556] duration-200" />
+        </a>
         <NavLink to="/">
-          <Logo className="h-20 w-20 fill-[#FD4556] hover:fill-[#ffff]" />
+          <SiValorant className="h-20 w-20 pt-5 fill-[#FD4556] hover:scale-125 duration-200" />
         </NavLink>
+        <a
+          className="absolute top-9 right-10 text-white/50 hover:text-white duration-200"
+          href="https://valorant-api.com/"
+          target="_blank"
+        >
+          API
+        </a>
       </div>
-      <ul className="pt-5 font-bold text-[#494949] justify-center items-center flex">
+      <ul className="pt-5 pb-5 font-bold text-[#494949] justify-center items-center flex">
         <NavLink
           className={({ isActive }) =>
             isActive
               ? 'active pr-20 md:pr-[300px]'
-              : 'hover:text-[#886565] duration-150 pr-20 md:pr-[300px]'
+              : 'hover:text-[#FD4556] duration-150 pr-20 md:pr-[300px]'
           }
           to="/"
         >
@@ -24,13 +38,14 @@ const Navbar = () => {
           className={({ isActive }) =>
             isActive
               ? 'active pl-20 md:pl-[300px]'
-              : 'hover:text-[#886565] duration-150 pl-20 md:pl-[300px]'
+              : 'hover:text-[#FD4556] duration-150 pl-20 md:pl-[300px]'
           }
           to="weapons"
         >
           ARSENAL
         </NavLink>
       </ul>
+      <div className="bg-[#FD4556] w-[100vh] md:w-[100vw] p-[1px]"></div>
     </nav>
   )
 }
