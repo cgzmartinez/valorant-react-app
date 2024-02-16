176e7209ee5b7e6b0bf9759f8687c685fea62b10
cgzmartinez <105771374+cgzmartinez@users.noreply.github.com>
2024-02-16 14:44:13 -0600

    Update App.jsx
    Finalized routing and added a loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,36 @@
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import AgentPage from './components/AgentPage'
+import Footer from './components/Footer'
+import Loading from './components/Loading' // Import the Loading component
+import Navbar from './components/Navbar'
+import WeaponsPage from './components/WeaponsPage'
+import { useState, useEffect } from 'react'
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    // Simulate data fetching (replace with your actual logic)
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 1000)
+  }, [])
 
   return (
-    <div>
-    </div>
+    <BrowserRouter>
+      {isLoading ? (
+        <Loading className="ease-out" />
+      ) : (
+        <div>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<AgentPage />} />
+            <Route path="/weapons" element={<WeaponsPage />} />
+          </Routes>
+          <Footer />
+        </div>
+      )}
+    </BrowserRouter>
   )
 }
 
