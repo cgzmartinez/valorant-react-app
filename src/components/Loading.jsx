4279c3dc0b9640f7e1bc913d623cad6fd28930e0
cgzmartinez <105771374+cgzmartinez@users.noreply.github.com>
2024-02-16 14:44:25 -0600

    Update Loading.jsx

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,11 +1,10 @@
-import Logo from '../assets/valorant-logo.svg?react'
-
+import { SiValorant } from 'react-icons/si'
 function Loading() {
   return (
     <div className="">
       {/* Your loading animation content (e.g., spinner, text) */}
       <div className="flex h-screen justify-center items-center" role="status">
-        <Logo className="fill-[#FD4556] h-[200px] w-auto" />
+        <SiValorant className="fill-[#FD4556] h-[200px] w-auto" />
       </div>
     </div>
   )
