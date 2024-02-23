4358ad89ebfcc9227a80d2591d7818f749d67779
cgzmartinez <105771374+cgzmartinez@users.noreply.github.com>
2024-02-22 22:06:06 -0600

    Update Footer.jsx
    Cleaning up code

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,12 @@
 const Footer = () => {
   return (
-    <div className="absolute  w-screen">
+    <div className="absolute w-screen">
       <div className="flex justify-center items-center bg-[#FD4556] mt-20">
         <p className="p-5 text-[10px] text-left text-white">
           This project site is not endorsed by Riot Games in any way. Riot
           Games, Valorant, Valorant-API, and all associated properties are
           trademarks or registered trademarks of Riot Games, Inc.
         </p>
-        <div></div>
       </div>
     </div>
   )
